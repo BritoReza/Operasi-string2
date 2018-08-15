e0894b9fe7ef280ccb5a3814fe39801f6a4da9d6
user <user@DESKTOP-BFBJO5R>
2018-08-15 22:18:39 +0700

    Initial Commit

---
+++ src/j11/Praktikum02.java
@@ -0,0 +1,16 @@
+package j11;
+
+public class Praktikum02 {
+    private static void tampilJudul(String identias){
+        System.out.println("Identitas : "+ identias);
+        
+        System.out.println("\nConvert Kalimat Alay Angka (Vokal Ke Angka)\n");
+    
+        String identitas = "Brito Reza Daud / X RPL 3 / 11";
+        
+        tampilJudul(identitas);
+        
+    }
+    
+    }
+
