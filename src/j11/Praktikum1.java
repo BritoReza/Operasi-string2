c67968fdb9aac35388c09a6e8da14abc82901002
user <user@DESKTOP-BFBJO5R>
2018-08-15 21:33:39 +0700

    Coba method length

--- src/j11/Praktikum1.java
+++ src/j11/Praktikum1.java
@@ -6,6 +6,17 @@ public class Praktikum1 {
         String identitas = "Brito Reza Daud / X RPL 3 / 11";
         System.out.println("Identitas : "+ identitas);
         
+        String x = "Operasi";
+        System.out.println("Isi variable x : "+ x);
+        System.out.println("\""+ x + "\" panjang karakter = "+ x.length());
+        
+        System.out.println("x adalah kosong : " + x.isEmpty());
+        
+        String y = "";
+        System.out.println("Isi variable y : "+ y);
+        System.out.println("y adalah kosong : " + y.isEmpty());
+        
+        
         
         
         
