f50b5797957d0df94a87084f3b37726c38db6827
user <user@DESKTOP-BFBJO5R>
2018-08-15 23:33:30 +0700

    Tambah tampil hasil

--- src/j11/ConvertVocal12Angka.java
+++ src/j11/ConvertVocal12Angka.java
@@ -14,9 +14,7 @@ public class ConvertVocal12Angka {
         
         tampilJudul(identitas);
         String kalimat = tampilInput();
-        String convert = vokal12Angka(kalimat);
-        tampilPerKata(kalimat, convert);
-        tampilHasil(convert);
+        
     }
     private static String tampilInput(){
         
@@ -29,5 +27,29 @@ public class ConvertVocal12Angka {
         return kalimat;
         
     }
-    String kalimat = tampilInput();
-}
+    
+    private static String vocal12Angka(String kaliamat) {
+        char [][] arConvert = {{'a','4'},{'i','1'},{'u','2'},{'e','3'},{'o','0'}};
+        
+        String kalimat = tampilInput();
+        kalimat = kalimat.toLowerCase();
+        for(int i=0;i<arConvert.length;i++)
+            kalimat = kalimat.replace(arConvert[i][0], arConvert[i][1]);
+        
+        return kalimat;
+    }
+    private static void tampilPerkata(String kalimat, String convert){
+        String[] arrKal = kalimat.split(" ");
+        String[] arrCon = convert.split(" ");
+        
+            for(int i=0; i<arrKal.length; i++)
+            {
+                System.out.println(arrKal[i]+" => "+arrCon[i]);
+            }
+    }
+    private static void tampilHasil(String convert)
+    {
+        System.out.println("Kalimat Alay Angka : " + convert);
+            }
+    }
+
